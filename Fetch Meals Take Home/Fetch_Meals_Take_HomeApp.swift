72636195b0d150874f8e9baa1f7f6e25c33c0add
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2024-09-18 00:07:18 -0400

    Initial Commit

---
+++ Fetch Meals Take Home/Fetch_Meals_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Meals_Take_HomeApp.swift
+//  Fetch Meals Take Home
+//
+//  Created by Chad-Michael Muirhead on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Meals_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
