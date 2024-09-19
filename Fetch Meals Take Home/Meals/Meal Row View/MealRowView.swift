c65ed75e4f83a5bbcdcd5ce3df204a4d3ca011ba
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2024-09-18 23:04:25 -0400

    feat: Added basic Meal Details view & Meal Details API Also added MealRowView for each row in the list of meals

---
+++ Fetch Meals Take Home/Meals/Meal Row View/MealRowView.swift
@@ -0,0 +1,18 @@
+//
+//  MealRowView.swift
+//  Fetch Meals Take Home
+//
+//  Created by Chad-Michael Muirhead on 9/18/24.
+//
+
+import SwiftUI
+
+struct MealRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealRowView()
+}
