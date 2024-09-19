ea55b4d559cd452b2ffb7a7a637f50ec9225095f
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2024-09-18 21:21:05 -0400

    setup folders and views for app

---
+++ Fetch Meals Take Home/Meals/MealsView.swift
@@ -0,0 +1,18 @@
+//
+//  MealsView.swift
+//  Fetch Meals Take Home
+//
+//  Created by Chad-Michael Muirhead on 9/18/24.
+//
+
+import SwiftUI
+
+struct MealsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealsView()
+}
---
+++ Fetch Meals Take Home/Services/MealsService.swift
@@ -0,0 +1,8 @@
+//
+//  MealsService.swift
+//  Fetch Meals Take Home
+//
+//  Created by Chad-Michael Muirhead on 9/18/24.
+//
+
+import Foundation
